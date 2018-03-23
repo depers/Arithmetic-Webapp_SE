3210a84dea568b0b17d45e0bea0bcfbddaa526d2
depers <dev_fengxiao@foxmail.com>
2018-03-23 22:09:30 +0800

    init project

---
+++ src/main/java/cn/bravedawn/airthmeticwebappse/AirthmeticwebappseApplication.java
@@ -0,0 +1,12 @@
+package cn.bravedawn.airthmeticwebappse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirthmeticwebappseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirthmeticwebappseApplication.class, args);
+	}
+}
